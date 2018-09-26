e186a510d9e15b94addd81cbd70af1ad0b90bb24
MacAttack79 <jmcintosh1979@gmail.com>
2018-09-26 15:02:33 -0400

    updated JS code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,81 +1,101 @@
+// $(document).ready(function() {
+
 var topics = ["Dog", "Cat", "Rabbit", "Hamster", "Goldfish", "Bird", "Turtle", "Pig", "Horse"];
 
 
-$("#animal-btns").on("click", function() {
+//Action item for which "animal button" is being selected.
+  $("#animal-btns").on("click", ".animal-btn", function() {
 
-  var animal = $(this).attr("data-animal");
-  console.log($(this));
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=tCRu63tckjEf9LvbDtzw3mzWDwlDZrp1&limit=10";
+    var animal = $(this).attr("data-animal");
+    console.log(animal);
+      
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=tCRu63tckjEf9LvbDtzw3mzWDwlDZrp1&limit=10";
 
-  $.ajax({
-    url:queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(queryURL);
-    console.log(response);
+    $.ajax({
+      url:queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(queryURL);
 
-    var results = response.data;
+      var results = response.data;
+      console.log(results);
 
-    for (var i = 0; i < results.length; i++) {
-      if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-        // creates a div to hold the images received
-        var newImg = $("<div>");
-        
-        // gets the rating of the image being received to display
-        var rating = results[i].rating;
-        var imgRating = $("<p>").text("Rating: " + rating);
-        
-        // gets the image being received to display, both still and animated version.  Also adds "giphy" class.
-        var animalGiphy = $("<img>");
-        animalGiphy.attr({"src":results[i].images.original_still.url, "data-still":results[i].images.original_still.url, "data-animate": results[i].images.fixed_height.url, "data-state":"still", "class":"giphy"});
-        
-        // appends newly created div with the rating and image
-        newImg.append(imgRating);
-        newImg.append(animalGiphy);
-        
-        // displayes the rating/image on the DOM
-        $("#animal-giphys").prepend(newImg);
+      // remove previously selected Giphys
+      $("#animal-giphys").empty();
+
+      for (var i = 0; i < results.length; i++) {
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+          // creates a div to hold the images received
+          var newImg = $("<div>");
+          
+          // gets the rating of the image being received to display
+          var rating = results[i].rating;
+          var imgRating = $("<p>").text("Rating: " + rating);
+          
+          // gets the image being received to display, both still and animated version.  Also adds "giphy" class.
+          var animalGiphy = $("<img>");
+          animalGiphy.addClass("giphy")
+          animalGiphy.attr({
+            "src":results[i].images.fixed_height_still.url, 
+            "data-still":results[i].images.fixed_height_still.url, 
+            "data-animate": results[i].images.fixed_height.url, 
+            "data-state":"still"});
+          
+          // appends newly created div with the rating and image
+          newImg.append(imgRating);
+          newImg.append(animalGiphy);
+          
+          // displayes the rating/image on the DOM
+          $("#animal-giphys").prepend(newImg);
+        }
       }
+    });
+  });
+
+  // Change image status to/from "Still" & "Animate"
+  $("#animal-giphys").on("click", ".giphy", function() {
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } 
+    else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
     }
   });
-});
+    
+  // Function to create the buttons from Array
+  function renderButtons() {
+    $("#animal-btns").empty();
 
+    // Goes through Array to create each individual button
+    for (var j = 0; j < topics.length; j++) {
+      var newBtn = $("<button>");
+      newBtn.addClass("animal-btn");
+      newBtn.attr("data-animal", topics[j]);
+      newBtn.text(topics[j]);
 
-$(".giphy").on("click", function() {
-  var state = $(this).attr("data-state");
+      // addes button to the DOM to be displayed
+      $("#animal-btns").append(newBtn);
+    }
+  };
 
-  if (state === "still") {
-    $(this).attr("src", $(this).attr("data-animate"));
-    $(this).attr("data-state", "animate");
-  } 
-  else {
-    $(this).attr("src", $(this).attr("data-still"));
-    $(this).attr("data-state", "still");
-  }
-});
-  
+  //Create new button with Users Input
+  $("#add-animal").on("click", function(e) {
+    e.preventDefault();
 
-function renderButtons() {
-  $("#animal-btns").empty();
+    // takes user input and pushes to Array of values
+    var newAnimal = $("#animal-input").val().trim();
+    topics.push(newAnimal);
 
-  for (var j = 0; j < topics.length; j++) {
-    var newBtn = $("<button>");
-    newBtn.addClass("animal-btn");
-    newBtn.attr("data-animal", topics[j]);
-    newBtn.text(topics[j]);
-    $("#animal-btns").append(newBtn);
-  }
-};
+    // Resets the text value to 'empty"
+    $("#animal-input").val("");
 
+    renderButtons();
+  });
 
-$("#add-animal").on("click", function(e) {
-  e.preventDefault();
-
-  var newAnimal = $("#animal-input").val().trim();
-  topics.push(newAnimal);
 
   renderButtons();
-});
-
-
-renderButtons();
+// })
